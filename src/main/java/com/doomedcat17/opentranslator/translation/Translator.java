9a97677eb5ba108c95a944bd3ead23519db39b42
Piotr Bojczewski <piotr200310@outlook.com>
2022-07-29 16:35:27 +0200

    init

---
+++ src/main/java/com/doomedcat17/opentranslator/translation/Translator.java
@@ -0,0 +1,81 @@
+package com.doomedcat17.opentranslator.translation;
+
+import com.doomedcat17.opentranslator.exception.TranslationException;
+import com.doomedcat17.opentranslator.language.Language;
+import com.doomedcat17.opentranslator.translation.mapper.TranslationBodyMapper;
+import com.doomedcat17.opentranslator.translation.provider.ParametersProvider;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+public class Translator {
+
+    private final String GOOGLE_ENDPOINT_URL = "https://translate.googleapis.com/translate_a/single";
+    private final TranslationBodyMapper translationBodyMapper = new TranslationBodyMapper();
+
+    private HttpClient client;
+
+    public Translator() {
+        this.client = HttpClient
+                .newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+    }
+
+    public Translator(HttpClient client) {
+        this.client = client;
+    }
+
+    public Translation translate(String sourceText, Language targetLanguage) {
+        return translate(sourceText, Language.AUTO, targetLanguage);
+    }
+    public Translation translate(String sourceText, Language sourceLanguage, Language targetLanguage) {
+        try {
+            URI uri = URI.create(attachParameters(sourceText, sourceLanguage, targetLanguage));
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .version(HttpClient.Version.HTTP_2)
+                    .uri(uri)
+                    .GET()
+                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+                    .build();
+            HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() != 200) {
+                throw new TranslationException(response.statusCode()+" status code");
+            }
+            Translation translation = translationBodyMapper.map(response.body());
+            translation.setSourceText(sourceText);
+            translation.setRawData(response.body());
+            translation.setTargetLanguage(targetLanguage);
+            translation.setUrl(uri);
+            return translation;
+        }
+        catch (TranslationException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new TranslationException(e);
+        }
+    }
+
+
+
+    private String attachParameters(String text, Language sourceLanguage, Language targetLanguage) {
+        StringBuilder sb = new StringBuilder(GOOGLE_ENDPOINT_URL+"?");
+        ParametersProvider.params.forEach((key, value) ->
+                sb.append(key).append("=").append(value).append("&"));
+        ParametersProvider.goofyParameters.forEach(value ->
+                sb.append("dt").append("=").append(value).append("&"));
+        sb.append("sl=").append(sourceLanguage.id);
+        sb.append("&tl=").append(targetLanguage.id);
+        sb.append("&hl=").append(targetLanguage.id);
+        sb.append("&q=").append(URLEncoder.encode(text, StandardCharsets.UTF_8));
+        return sb.toString();
+    }
+
+    public void setClient(HttpClient client) {
+        this.client = client;
+    }
+}
